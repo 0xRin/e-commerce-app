41cf5f563c4ddde1f4692adffbf79b837bb563ba
0xRin <73491185+0xRin@users.noreply.github.com>
2023-01-10 12:44:35 -0500

    navbar and general layout

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,93 @@
+import React from "react";
+import styled from "styled-components";
+import { Search, ShoppingCartOutlined } from "@mui/icons-material/";
+import { Badge } from "@mui/material";
+
+const Navbar = () => {
+  return (
+    <Container>
+      <Wrapper>
+        <Left>
+          <Language>EN</Language>
+          <SearchContainer>
+            <Input />
+            <Search style={{ color: "gray", fontSize: 16 }} />
+          </SearchContainer>
+        </Left>
+        <Center>
+          <Logo>LAMA.</Logo>
+        </Center>
+        <Right>
+          <MenuItem>REGISTER</MenuItem>
+          <MenuItem>SIGN IN</MenuItem>
+          <MenuItem>
+            <Badge badgeContent={4} color="primary">
+              <ShoppingCartOutlined />
+            </Badge>
+          </MenuItem>
+        </Right>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Navbar;
+
+const Container = styled.div`
+  height: 60px;
+`;
+
+const Wrapper = styled.div`
+  padding: 10px 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
+`;
+
+const Left = styled.div`
+  flex: 1;
+  display flex;
+  align-items: center;
+`;
+
+const SearchContainer = styled.div`
+  border: 0.5px solid lightgray;
+  display: flex;
+  align-items: center;
+  margin-left: 25px;
+  padding: 5px;
+`;
+
+const Input = styled.input`
+  border: none;
+  outline: none;
+`;
+
+const Language = styled.span`
+  font-size 14px;
+  cursor: pointer;
+`;
+
+const Center = styled.div`
+  flex: 1;
+  text-align: center;
+`;
+
+const Logo = styled.div`
+  font-weight: bold;
+  font-size: 36px;
+`;
+
+const Right = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  gap: 10px;
+`;
+
+const MenuItem = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+`;
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import styled from "styled-components";
+import Announcement from "../components/Announcement";
+import Slider from "../components/Slider";
+
+const Home = () => {
+  return (
+    <>
+      <Announcement />
+      <Navbar />
+      <Slider />
+    </>
+  );
+};
+
+export default Home;
+
+// styled components
