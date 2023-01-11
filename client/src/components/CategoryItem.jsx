1a158667cd9d424bac6914498ff09f63ab2be48a
0xRin <73491185+0xRin@users.noreply.github.com>
2023-01-11 13:59:07 -0500

    feat: add category section

---
+++ client/src/components/CategoryItem.jsx
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+
+const CategoryItem = ({ item }) => {
+  return (
+    <Container>
+      <Image src={item.img} />
+      <Info>
+        <Title>{item.title}</Title>
+        <Button>SHOP NOW</Button>
+      </Info>
+    </Container>
+  );
+};
+
+export default CategoryItem;
+
+const Container = styled.div`
+  flex: 1;
+  margin: 3px;
+  height: 70vh;
+  position: relative;
+`;
+const Image = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`;
+const Info = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+`;
+const Button = styled.button`
+  cursor: pointer;
+  font-weight: 400;
+`;
+const Title = styled.h1`
+  color: white;
+  text-align: center;
+`;
