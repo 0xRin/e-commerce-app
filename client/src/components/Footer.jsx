4b5e588abb181a98177bddd5acfbffe68b9fdce4
0xRin <73491185+0xRin@users.noreply.github.com>
2023-01-11 20:09:03 -0500

    feat: add footer

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,135 @@
+import {
+  Facebook,
+  Instagram,
+  Mail,
+  Phone,
+  Reddit,
+  Room,
+  Twitter,
+} from "@mui/icons-material";
+import styled from "styled-components";
+
+const Footer = () => {
+  return (
+    <Container>
+      <Left>
+        <Logo>LAMA.</Logo>
+        <Desc>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae,
+          corporis!
+        </Desc>
+        <SocialContainer>
+          <SocialIcon color="3B5999">
+            <Facebook />
+          </SocialIcon>
+          <SocialIcon color="55ACEE">
+            <Twitter />
+          </SocialIcon>
+          <SocialIcon color="E4405F">
+            <Instagram />
+          </SocialIcon>
+          <SocialIcon color="E60023">
+            <Reddit />
+          </SocialIcon>
+        </SocialContainer>
+      </Left>
+      <Center>
+        <Title>Useful Links</Title>
+        <List>
+          <ListItem>Home</ListItem>
+          <ListItem>Cart</ListItem>
+          <ListItem>Man Fashion</ListItem>
+          <ListItem>Woman Fashion</ListItem>
+          <ListItem>Accessories</ListItem>
+          <ListItem>My Account</ListItem>
+          <ListItem>Track My Orders</ListItem>
+          <ListItem>Wishlist</ListItem>
+          <ListItem>About</ListItem>
+          <ListItem>Terms</ListItem>
+        </List>
+      </Center>
+      <Right>
+        <Title>Contact</Title>
+        <ContactItem>
+          <Room />
+          323 Texas Rd, South Hampton 48382
+        </ContactItem>
+        <ContactItem>
+          <Phone />
+          +1 345 32 34
+        </ContactItem>
+        <ContactItem>
+          <Mail />
+          contact@gmail.com
+        </ContactItem>
+        <Payment src="https://i.ibb.co/Qfvn4z6/payment.png" />
+      </Right>
+    </Container>
+  );
+};
+
+export default Footer;
+
+const Container = styled.div`
+  display: flex;
+`;
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+`;
+
+const Center = styled.div`
+  flex: 1;
+  padding: 20px;
+`;
+
+const Right = styled.div`
+  flex: 1;
+  padding: 20px;
+`;
+
+const ContactItem = styled.div`
+  margin-bottom: 20px;
+  display: flex;
+  align-items: center;
+  display: flex;
+  gap: 10px;
+`;
+const Payment = styled.img`
+  width: 50%;
+`;
+
+const Title = styled.h3`
+  margin-bottom: 30px;
+`;
+const List = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+`;
+const ListItem = styled.li`
+  width: 50%;
+  margin-bottom: 10px;
+`;
+
+const Logo = styled.h1``;
+const Desc = styled.p`
+  margin: 20px 0px;
+`;
+const SocialIcon = styled.div`
+  color: white;
+  height: 40px;
+  width: 40px;
+  border-radius: 50%;
+  background-color: #${(props) => props.color};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const SocialContainer = styled.div`
+  display: flex;
+`;
