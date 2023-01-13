ea09fd2495e735f5d91375976f49725c314d09ae
0xRin <73491185+0xRin@users.noreply.github.com>
2023-01-13 14:00:46 -0500

    chore: add json middleware and baseurl const

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ require("express-async-errors");
 
 // local imports
 const startUpServer = require("./util/startUpServer");
+const userRouter = require("./routes/userRouter");
 
 //instantiate app
 const app = express();
@@ -12,10 +13,13 @@ dotenv.config();
 
 // Port
 const port = process.env.PORT || 3000;
+const baseUrl = "/api";
 
 // middleware
+app.user(express.json());
 
 // routes
+app.use(`${baseUrl}/users`, userRouter);
 
 // start server
 startUpServer(port, app);
