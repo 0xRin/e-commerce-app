fcf75df5dbba9d869fe762bea3eb4b115914aa1f
0xRin <73491185+0xRin@users.noreply.github.com>
2023-01-13 22:53:13 -0500

    chore(middleware): add role authentication

--- backend/middleware/authenticateUserMiddleware.js
+++ backend/middleware/authenticateUserMiddleware.js
@@ -2,18 +2,32 @@ const jwt = require("jsonwebtoken");
 const UnathorizedError = require("../errors/UnauthorizedError");
 const ForbiddenError = require("../errors/ForbiddenError");
 
-const authenticateUserMiddleware = (res, req, next) => {
-  const authHeader = req.headers.accessToken;
+const authenticateUserMiddleware = (req, res, next) => {
+  const authHeader = req.headers.authorization || req.headers.Authorization;
 
-  if (!authHeader)
-    throw new UnathorizedError("Unathorized access: cannot access this route");
+  if (!authHeader?.startsWith("Bearer")) {
+    throw new UnathorizedError("Unathorized: No Authorization Header");
+  }
+
+  const token = authHeader.split(" ")[1];
 
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-    if (err) throw new ForbiddenError("Auth Token expired, please login");
+    if (err) throw new ForbiddenError("Auth Token invalid, please login");
 
     req.user = user;
+    console.log(user);
     next();
   });
 };
 
-module.exports = authenticateUserMiddleware;
+const authenticateRoleMiddleware = (req, res, next) => {
+  authenticateUserMiddleware(req, res, () => {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+      next();
+    } else {
+      throw new UnathorizedError("Cannot change another user's info");
+    }
+  });
+};
+
+module.exports = { authenticateUserMiddleware, authenticateRoleMiddleware };
